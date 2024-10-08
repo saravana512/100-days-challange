d3b3655a3dfec9e098fe62e7fc51a95566da88de
saravanakumar.arumugam <saravanakumar.arumugam@aroopatech.com>
2024-10-08 13:43:52 +0530

    with commented and sample pgm

--- string,array_splice_slice.js
+++ string,array_splice_slice.js
@@ -1,20 +1,25 @@
-String:
+// String:
+// Not affect the original string
+
 slice(indexStart)
 slice(indexStart, indexEnd)
 
 
-Array:
+// Array:
+
+// slice Not affect the original string
 slice()
 slice(start)
 slice(start, end)
 
+// slice  affect the original string
 splice(start)
 splice(start, deleteCount)
 splice(start, deleteCount, item1)
 splice(start, deleteCount, item1, item2)
 splice(start, deleteCount, item1, item2, /* …, */ itemN)
 
-
+// slice  affect the original string
 toSpliced(start)
 toSpliced(start, deleteCount)
 toSpliced(start, deleteCount, item1)
@@ -24,11 +29,17 @@ toSpliced(start, deleteCount, item1, item2, /* …, */ itemN)
 
 
 
-sample: 
+// sample: 
 
-const a = [1,2,3,4,5,6, 7]
+// const a = [1,2,3,4,5,6, 7]
 
-// console.log(a.slice(2, 5))
-console.log(a.splice(2, 4, 'demo', 'arum'))
+// // console.log(a.slice(2, 5))
+// console.log(a.splice(2, 4, 'demo', 'arum'))
 
-console.log(a)
+// console.log(a)
+
+// const a = 'this is the saravana kumar 123';
+
+// console.log(a)
+// console.log(a.slice(1, 3))
+// console.log(a)
