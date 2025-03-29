4d5286e1c4f5c94314f34d8485064955d6ac757d
Saravanakumar <saravanakumara512@gmail.com>
2025-03-29 09:04:54 +0530

    Pattern learn

--- DSA/startPattern/leftsidePramide.js
+++ DSA/startPattern/leftsidePramide.js
@@ -19,3 +19,37 @@ function leftsidePramide(n) {
 }
 
 leftsidePramide(10);
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+for (let i = 1; i <= 5; i++) {
+  let str = "";
+  for (let j = 1; j <= i; j++) {
+    str += `${j}`;
+  }
+  console.log(str);
+}
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+for (let i = 1; i <= 5; i++) {
+  let str = "";
+  for (let j = 1; j <= i; j++) {
+    str += `${i}`;
+  }
+  console.log(str);
+}
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
---
+++ tmrw_questions.js
@@ -0,0 +1,23 @@
+// Q1
+
+// You are given a linked list.
+// Starting from the middle element, print the elements in a spiral anticlockwise direction.
+// If length of elements is even, start from second of the two middle elements.
+
+// Example -
+// LL = 18,4,9,5,1,56,8,40
+// Output = 1,5,56,9,8,4,40,18
+
+// LL = 5,3,6,4,8,2,7
+// Output = 4,6,8,3,2,5,7
+
+// Q2
+
+// Given a string s, find the length of the longest
+// substring
+// without repeating characters.
+
+// example - abcdabefgh
+// answer - 8, i.e cdabefgh
+
+const str = "abcdabefgh";
